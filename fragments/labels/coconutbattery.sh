e98f0ee772158d90677eab643142a92f862bddf1
Thomas Martin <42557105+thomasrmartin@users.noreply.github.com>
2022-02-14 16:37:51 -0500

    Create coconut battery label
    https://www.coconut-flavour.com/coconutbattery/

---
+++ fragments/labels/coconutbattery.sh
@@ -0,0 +1,6 @@
+coconutbattery)
+    name="coconutBattery"
+    type="zip"
+    downloadURL="https://coconut-flavour.com/downloads/coconutBattery_latest.zip"
+    expectedTeamID="R5SC3K86L5"
+    ;;
