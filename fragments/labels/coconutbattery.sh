5f3faa740984bc3b8eea495924246a748c97ba87
Thomas Martin <42557105+thomasrmartin@users.noreply.github.com>
2022-02-23 16:17:22 -0500

    Added appNewVersion

--- fragments/labels/coconutbattery.sh
+++ fragments/labels/coconutbattery.sh
@@ -2,5 +2,6 @@ coconutbattery)
     name="coconutBattery"
     type="zip"
     downloadURL="https://coconut-flavour.com/downloads/coconutBattery_latest.zip"
+    appNewVersion=$(curl -fs https://www.coconut-flavour.com/coconutbattery/ | grep "<title>" | sed -e  's/.*coconutBattery \(.*\) - by coconut-flavour.co.*/\1/')
     expectedTeamID="R5SC3K86L5"
     ;;
